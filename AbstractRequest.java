4bec47491af8ef17be592ff6be45624a9e1900db
Harish Gyanani <harish@netleon.com>
2016-07-29 12:53:40 +0530

    hhhfgg

--- AbstractRequest.java
+++ AbstractRequest.java
@@ -4,10 +4,10 @@ package com.commonutils;
  * Created by b on 6/22/2016.
  */
 
-public abstract class AbstractRequest implements IRequest {
+public abstract class AbstractRequest implements com.commonutils.IRequest {
     @Override
     public boolean shouldShowLoading() {
-        return Loading.SHOW_LOADING;
+        return com.commonutils.Loading.SHOW_LOADING;
     }
 
     @Override
